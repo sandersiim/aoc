91c63b35777345b76f8c4095387c9c4ea390492f
Sander Siim <sander.siim@pactum.com>
2021-12-04 15:44:13 +0200

    Day 3, part2 solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,26 +1,31 @@
 import org.assertj.core.api.Assertions.assertThat
 
 fun main() {
-  fun part1(input: List<String>): UInt {
-    println("Input size: ${input.size}")
-    val bitLength = input.first().length
+  fun getMostCommonBits(input: List<CharArray>): List<Char> {
+    val bitLength = input.first().size
 
-    println("Bit length: $bitLength")
     val positionalSums = Array(bitLength) { 0 }
     input.forEach { bitString ->
       bitString
-        .toCharArray()
         .forEachIndexed { index, bit ->
           positionalSums[index] += (bit - '0')
         }
     }
 
-    val gammaBitString = positionalSums.map {
-      when(it > input.size / 2) {
+    return positionalSums.map {
+      when(it >= input.size / 2.0) {
         true -> '1'
         false -> '0'
       }
-    }.joinToString("")
+    }
+  }
+
+  fun part1(input: List<String>): UInt {
+    println("Input size: ${input.size}")
+    val bitLength = input.first().length
+    println("Bit length: $bitLength")
+
+    val gammaBitString = getMostCommonBits(input.map { it.toCharArray() }).joinToString("")
     println("Gamma rate bitstring: $gammaBitString")
 
     val gammaRate = gammaBitString.toUInt(2)
@@ -35,19 +40,42 @@ fun main() {
   }
 
   fun part2(input: List<String>): UInt {
-    return 0u
+    val bitLength = input.first().length
+    println("Bit length: $bitLength")
+
+    var oxygenRatingCandidates = input.map { it.toCharArray() }
+    var co2RatingCandidates = input.map { it.toCharArray() }
+    (0 until bitLength).forEach { bitIndex ->
+      if (oxygenRatingCandidates.size > 1) {
+        oxygenRatingCandidates = oxygenRatingCandidates.filter {
+          it[bitIndex] == getMostCommonBits(oxygenRatingCandidates)[bitIndex]
+        }
+      }
+
+      if (co2RatingCandidates.size > 1) {
+        co2RatingCandidates = co2RatingCandidates.filter {
+          it[bitIndex] != getMostCommonBits(co2RatingCandidates)[bitIndex]
+        }
+      }
+    }
+
+    val oxygenRating = oxygenRatingCandidates.first().joinToString("").toUInt(2)
+    val co2Rating = co2RatingCandidates.first().joinToString("").toUInt(2)
+    println("Oxygen rating: $oxygenRating, co2 rating: $co2Rating")
+
+    return oxygenRating * co2Rating
   }
 
   val testInput = readInput("day3_test")
   val testResult1 = runSolution("Part1 test") { part1(testInput) }
   assertThat(testResult1).isEqualTo(198u)
 
-  val testResult2 = runSolution("Part2 test") { part2(testInput) }
-  assertThat(testResult2).isEqualTo(230u)
-
   val input = readInput("day3_input")
 
   runSolution("Part1") { part1(input) }
+
+  val testResult2 = runSolution("Part2 test") { part2(testInput) }
+  assertThat(testResult2).isEqualTo(230u)
 
   runSolution("Part2") { part2(input) }
 }
